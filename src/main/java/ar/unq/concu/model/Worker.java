22ddff20375b8e67a730ace6f6e4793c8e11cca2
zolezzi <cezcardozo23@gmail.com>
2018-11-01 01:44:11 -0300

    Refactor with whiel in the worker

--- src/main/java/ar/unq/concu/model/Worker.java
+++ src/main/java/ar/unq/concu/model/Worker.java
@@ -5,14 +5,18 @@ import java.util.List;
 
 public class Worker extends Thread{
 	
+	private int id;
 	private Buffer buffer;
 	private VectorManagerResult vectorManagerResult;
 	private SeqVector seqVector;
 	private Method[] methods;
 	
-	public Worker(Buffer buffer, VectorManagerResult vectorManagerResult) {
+	public Worker(int id, Buffer buffer, VectorManagerResult vectorManagerResult) {
+		
+		this.id = id;
 		this.buffer = buffer;
 		this.vectorManagerResult = vectorManagerResult;
+	
 	}
 
 	public Buffer getBuffer() {
@@ -33,7 +37,7 @@ public class Worker extends Thread{
 	
 	private void loadValues(SeqVector seqVector, List<Double> values) {
 		for (int i = 0; i < values.size(); i++) {
-			seqVector.set(values.get(i));
+			seqVector.setWithPosition(i,values.get(i));
 		}
 	}
 	
@@ -56,23 +60,45 @@ public class Worker extends Thread{
 	@Override
 	public void run () {
 
-		while ( !buffer.getVectorTasksIsEmpty()) {
+		while ( true) {
 		
 			try {
 				
 				VectorTasks tasks = buffer.pop();
 				
-				seqVector = new SeqVector(tasks.getPositions().size());
+				double value;
 				
-				loadValues(seqVector, tasks.getValues());
-				
-				Method method = getMethod(tasks.getOperationName());
-				
-				double value = (double) method.invoke(seqVector);
+				if(tasks.getSeqVector() == null) {
 
-				VectorResult vectorResult = new VectorResult(value, tasks.getOperationName());
+					seqVector = new SeqVector(tasks.getPositions().size());
+					
+					loadValues(seqVector, tasks.getValues());
+					
+					Method method = getMethod(tasks.getOperationName());
+					
+					value = (double) method.invoke(seqVector);
+					
+					System.out.println("Valor que resolvio el invoke: " + value + " Por el THREAD:" + this.id);
+
+					VectorResult vectorResult = new VectorResult(value, tasks.getOperationName());
 				
-				vectorManagerResult.getResults().add(vectorResult);
+					vectorManagerResult.getResults().add(vectorResult);
+				
+				}else {
+					
+					seqVector = tasks.getSeqVector();
+					
+					System.out.println("WORKER " + tasks.getOperationName() + " ID: " + this.id);
+					
+					Method method = getMethod(tasks.getOperationName());
+					
+					method.invoke(seqVector, tasks.getPositions().get(0), tasks.getValues().get(0));
+					
+					System.out.println("POSITION: " + tasks.getPositions().get(0) + " VALUE: " + tasks.getValues().get(0));
+				
+				}
+				
+				
 			
 			} catch (Exception e) {
 				
